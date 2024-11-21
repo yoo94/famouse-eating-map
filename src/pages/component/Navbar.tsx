a70a1844dcdb931d4069b38ce54a5a74fa3966ac
yoo94 <ahy0119@naver.com>
2024-11-22 01:03:21 +0900

    - feature 네비게이션 바

--- src/pages/component/Navbar.tsx
+++ src/pages/component/Navbar.tsx
@@ -1,17 +1,35 @@
 import { useState } from "react";
 import Link from "next/link";
 
+import { BiMenu } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
+
 export default function Navbar(){
     const [isOpen, setIsOpen] = useState(false);
     return(
         <>
           <div className="navbar">
-            <div className="navbar_logo">nextmap</div>
-            <div className="navbar_list">
-                <Link className="navbar_list--item" href="/stores">맛집  목록</Link>
-                <Link className="navbar_list--item" href="/stores/new">맛집  등록</Link>
-                <Link className="navbar_list--item" href="/stores/1">맛집  가게</Link>
-                <Link className="navbar_list--item" href="/users/login">로그인</Link>
+            <div className="navbar__logo">nextmap</div>
+            <div className="navbar__list">
+                <Link className="navbar__list--item" href="/stores">맛집  목록</Link>
+                <Link className="navbar__list--item" href="/stores/new">맛집  등록</Link>
+                <Link className="navbar__list--item" href="/stores/1">맛집  가게</Link>
+                <Link className="navbar__list--item" href="/users/login">로그인</Link>
+            </div>
+            <div role="presentation" className="navbar__button" onClick={()=> setIsOpen((val) => !val)}>
+              {isOpen ? <AiOutlineClose/> : <BiMenu/>}
+            </div>
+          </div>
+          {/* mobile */}
+          <div className="navbar--mobile">
+            <div className="navbar_list--mobile">
+                <Link className="navbar__list--item--mobile" href="/stores">맛집  목록</Link>
+                <Link className="navbar__list--item--mobile" href="/stores/new">맛집  등록</Link>
+                <Link className="navbar__list--item--mobile" href="/stores/1">맛집  가게</Link>
+                <Link className="navbar__list--item--mobile" href="/users/login">로그인</Link>
+            </div>
+            <div role="presentation" className="navbar__button" onClick={()=> setIsOpen((val) => !val)}>
+              {isOpen ? <AiOutlineClose/> : <BiMenu/>}
             </div>
           </div>
         </>
