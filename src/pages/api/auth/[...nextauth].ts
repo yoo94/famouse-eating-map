a15705108ed4ccdf85d2e5a08393231f8b0e8a80
yoo94 <ahy0119@naver.com>
2024-11-30 01:09:15 +0900

    - feat: custom login화면 및 카카오

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,7 +3,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 import GoogleProvider from "next-auth/providers/google";
 import NaverProvider from "next-auth/providers/naver";
-import { signIn } from "next-auth/react";
+import KakaoProvider from "next-auth/providers/kakao";
 
 const prisma = new PrismaClient()
 
@@ -19,8 +19,15 @@ export const authOptions = {
         NaverProvider({
             clientId: process.env.NAVER_CLIENT_ID || "",
             clientSecret: process.env.NAVER_CLIENT_SECRET || ""
+        }),
+        KakaoProvider({
+            clientId: process.env.KAKAO_CLIENT_ID || "",
+            clientSecret: process.env.KAKAO_CLIENT_SECRET || ""
         })
     ],
+    pages: {
+        signIn: "/users/login",
+    },
 }
 
 export default NextAuth(authOptions)
--- src/pages/component/Navbar.tsx
+++ src/pages/component/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
     <>
       <div className="navbar">
         <Link className="navbar__logo" href="/">
-          nextmap
+          Famouse Korea Map
         </Link>
         <div className="navbar__list">
           <Link href="/stores" className="navbar__list--item">
